c6d1846474bcad9f45146d206e549a54beb7383a
GuillaumeCoutelet <guillaume.coutelet@gmail.com>
2018-02-14 15:41:53 +0100

    Master : Selenium Grid - Première tentative

--- jpetstore-test/src/test/java/fr/autom/jpetstore_test/PetStoreTest.java
+++ jpetstore-test/src/test/java/fr/autom/jpetstore_test/PetStoreTest.java
@@ -2,6 +2,9 @@ package fr.autom.jpetstore_test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +15,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class PetStoreTest {
 
@@ -19,12 +24,15 @@ public class PetStoreTest {
 	String browser;
 	
 	@Before
-	public void setUp() {
-		//Commentaire
-		//Commentaire2
+	public void setUp() throws MalformedURLException {
 		browser = System.getProperty("browser");
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("browserName", browser);
+		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
+		
+		
 
-		if (browser.equals("firefox")) {
+		/*if (browser.equals("firefox")) {
 			FirefoxOptions options = new FirefoxOptions().setProfile(new FirefoxProfile());
 			options.addPreference("browser.tabs.remote.autostart", false);
 			System.setProperty("webdriver.gecko.driver",
@@ -35,7 +43,7 @@ public class PetStoreTest {
 			System.setProperty("webdriver.chrome.driver",
 					"C:\\Users\\Formation\\OWASP ZAP\\webdriver\\windows\\32\\chromedriver.exe");
 			driver = new ChromeDriver();
-		}
+		}*/
 
 		driver.get("http://localhost:8085/jpetstore/actions/Catalog.action");
 	}
